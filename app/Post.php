130b8bb8560b9997430d9a09129224464126fd2b
yingjhen <s3a632059@student.ncut.edu.tw>
2019-11-06 16:27:02 +0800

    練習2-3 設定 Post.php的 fillable屬性

--- app/Post.php
+++ app/Post.php
@@ -9,4 +9,12 @@ class Post extends Model
     //
     //練習1-2
     protected $table='posts';
+
+    //練習2-3
+    protected $fillable=[
+        'title',
+        'content',
+        'is_feature'
+    ];
+
 }
