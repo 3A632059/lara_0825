21aa20f196da4b00ed78611c7bc7923decbe2edf
yingjhen <s3a632059@student.ncut.edu.tw>
2019-12-04 11:46:43 +0800

    練習7-3 增加關聯

--- app/Comment.php
+++ app/Comment.php
@@ -3,9 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Post;
 
 class Comment extends Model
 {
     //
     protected $fillable = ['content'];
+    
+    public function post()
+    {
+    return $this->belongsTo(Post::class);
+    }
+
 }
